ce7ff1d9dd3cd57ccf5620bbebaa7a102a110b87
ianwoolf <btw515wolf2@gmail.com>
2017-06-23 15:04:51 +0800

    update model

--- loda/alarm.go
+++ loda/alarm.go
@@ -8,23 +8,23 @@ import (
 
 type Alarm struct {
 	AlarmData    m.Alarm
-	BlockStack   int
+	BlockStep    int
 	MaxStackTime int
 }
 
 var (
-	defaultBlockStack = 5  // unit: minute   TODO
-	maxBlockTime      = 60 // unit: minute
+	defaultBlockStep = 5  // unit: minute   TODO
+	maxBlockTime     = 60 // unit: minute
 )
 
 func newAlarm(alarm m.Alarm) *Alarm {
 	output := Alarm{AlarmData: alarm}
 
 	var err error
-	if output.BlockStack, err = strconv.Atoi(alarm.BlockStack); err != nil || output.BlockStack < 1 {
-		output.BlockStack = defaultBlockStack
+	if output.BlockStep, err = strconv.Atoi(alarm.BlockStep); err != nil || output.BlockStep < 1 {
+		output.BlockStep = defaultBlockStep
 	}
-	if output.MaxStackTime, err = strconv.Atoi(alarm.MaxStackTime); err != nil || output.MaxStackTime < 10 {
+	if output.MaxStackTime, err = strconv.Atoi(alarm.MaxBlockTime); err != nil || output.MaxStackTime < 10 {
 		output.MaxStackTime = maxBlockTime
 	}
 	return &output
