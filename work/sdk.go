8a3a325650ead820ed0b58c0ae0cbecf03e25a1a
ianwoolf <btw515wolf2@gmail.com>
2017-09-07 17:09:10 +0800

    log status change via sdk

--- work/sdk.go
+++ work/sdk.go
@@ -28,12 +28,40 @@ func getRecieverInfo(revievers []string) []string {
 	return receiverInfoSplit[:i]
 }
 
-func logAlarm(name, ns, measurement, host, level string, reveivers []string, value float64) error {
+func sendToSDK(ms []m.Metric) error {
+	data, err := json.Marshal(ms)
+	if err != nil {
+		return err
+	}
+	return sdk.Post(config.GetConfig().Com.EventLogNs, data)
+}
+
+type sdkLogFunc interface {
+	// log every alarm when output.
+	LogAlarm(name, ns, measurement, host, level string, receives []string, value float64) error
+
+	// log new status.
+	LogNewStatus(alertMsg models.AlertMsg, level string, receives []string) error
+
+	// log status changed.
+	LogStatusChange(alertMsg models.AlertMsg, level string, receives []string, statusStartTime time.Time) error
+}
+
+// SdkLog log the event/status via sdk.
+type SdkLog struct {
+	sdkLogFunc
+}
+
+// global SdkLog obj
+var sdkLog SdkLog
+
+// newMetric make []m.Metric with the param.
+// NOTICE: newMetric not set Metric.Name.
+func (s *SdkLog) newMetric(name, ns, measurement, host, level string, receives []string, value float64) []m.Metric {
 	ms := make([]m.Metric, 1)
-	receiverList := getRecieverInfo(reveivers)
+	receiverList := getRecieverInfo(receives)
 
 	ms[0] = m.Metric{
-		Name:      "alert",
 		Timestamp: time.Now().Unix(),
 		Tags: map[string]string{
 			"alertname":   name,
@@ -44,43 +72,42 @@ func logAlarm(name, ns, measurement, host, level string, reveivers []string, val
 			"to":          strings.Join(receiverList, "\\,")},
 		Value: fmt.Sprintf("%.2f", value),
 	}
-
-	data, err := json.Marshal(ms)
-	if err != nil {
-		return err
-	}
-	return sdk.Post(config.GetConfig().Com.EventLogNs, data)
+	return ms
 }
 
-func logStatus(status models.Status) error {
-	ms := make([]m.Metric, 1)
-	ms[0] = m.Metric{
-		Name:      "status",
-		Timestamp: time.Now().Unix(),
-		Tags: map[string]string{
-			"measurement": status.Measurement,
-			"ns":          status.Ns,
-			"host":        status.Host,
-		},
-		// Value: fmt.Sprintf("%.2f", value),
+// setLastTime set last time string to ms.
+func (s *SdkLog) setNemToMetric(ms []m.Metric, name string) {
+	for i := range ms {
+		ms[i].Name = name
 	}
-	if status.Level == OK {
-		ms[0].Value = 0
-	} else {
-		ms[0].Value = 1
-	}
-
-	data, err := json.Marshal(ms)
-	if err != nil {
-		return err
-	}
-	return sdk.Post(config.GetConfig().Com.EventLogNs, data)
 }
 
-func logNewStatus(status models.Status) error {
-	return logStatus(status)
+// setLastTime set last time string to ms.
+func (s *SdkLog) setLastTime(ms []m.Metric, lastTimeStr string) {
+	for i := range ms {
+		ms[i].Tags["last"] = lastTimeStr
+	}
 }
 
-func logOneStatus(status models.Status) error {
-	return logStatus(status)
+// Event log the event via sdk.(v1) It is used when output a alarm.
+func (s *SdkLog) Event(name, ns, measurement, host, level string, receives []string, value float64) error {
+	ms := s.newMetric(name, ns, measurement, host, level, receives, value)
+	s.setNemToMetric(ms, "alert")
+	return sendToSDK(ms)
+}
+
+// NewStatus log a new status via sdkl.(v2)  maybe the event is the first alarm of this ns/alarm/host.
+func (s *SdkLog) NewStatus(name, ns, measurement, host, level string, receives []string, value float64) error {
+	ms := s.newMetric(name, ns, measurement, host, level, receives, value)
+	s.setNemToMetric(ms, "status")
+	s.setLastTime(ms, "-")
+	return sendToSDK(ms)
+}
+
+// StatusChange log a status change event via sdk.
+func (s *SdkLog) StatusChange(name, ns, measurement, host, level string, receives []string, value float64, statusStartTime time.Time) error {
+	ms := s.newMetric(name, ns, measurement, host, level, receives, value)
+	s.setNemToMetric(ms, "status")
+	s.setLastTime(ms, time.Now().Sub(statusStartTime).String())
+	return sendToSDK(ms)
 }
