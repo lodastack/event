a0dc996fc45c9c5b42207ff32e659497d07386a2
llussy <usanfeng@gmail.com>
2020-09-16 13:28:49 +0800

    add API judge

--- work/work.go
+++ work/work.go
@@ -206,9 +206,11 @@ func (w *Work) HandleEvent(ns, alarmversion string, eventData models.EventData) 
 		log.Debug("event data has no host, maybe cluster alarm.")
 	}
 
-	if loda.IsOfflineMachine(ns, host) || !loda.IsExist(ns, host) {
-		log.Warningf("ns %s hostname %s is offline, not alert", ns, host)
-		return nil
+	if !strings.HasPrefix(alarm.AlarmData.Measurement,"RUN.API.") {
+		if loda.IsOfflineMachine(ns, host) || !loda.IsExist(ns, host) {
+			log.Warningf("ns %s hostname %s is offline, not alert", ns, host)
+			return nil
+		}
 	}
 	ip, _ := loda.MachineIP(ns, host)
 
